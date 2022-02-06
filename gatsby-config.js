821aa39147ade4d3069b606394ee9e83752bedb1
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2022-02-06 22:08:44 +0100

    Added GTM

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,6 +52,23 @@ module.exports = {
         icon: `src/images/dtangerfors-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-WP8KW96",
+  
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+      },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
