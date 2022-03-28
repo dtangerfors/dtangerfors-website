3294550cb44b64d610368b4171ab29408a365d00
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2022-03-28 12:39:26 +0200

    Fixed dark mode issues on text

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -21,11 +21,11 @@ const ProjectCard = ({ item, index }) => (
           className="w-full h-full object-cover"
         />
       </figure>
-      <div className="flex flex-col justify-end flex-wrap pt-2 mt-4 md:mt-8 md:pt-4 lg:mt-0 pb-8 bg-transparent border-t border-neutral-900/10 dark:border-white/30 transition-all duration-300 ease-in-out lg:absolute lg:bottom-0 lg:left-0 lg:p-8 lg:w-full lg:h-full lg:border-none lg:bg-gradient-to-t from-black/40 to-transparent lg:translate-y-10 lg:group-hover:translate-y-0">
-        <h2 className="font-serif text-lg text-black lg:text-white lg:group-hover:text-black lg:dark:group-hover:text-white lg:border-b lg:group-hover:border-neutral-900/10 lg:dark:group-hover:border-white/30 border-white/30 lg:h-12 lg:mb-2">
+      <div className="flex flex-col justify-end flex-wrap pt-2 mt-4 md:mt-8 md:pt-4 lg:mt-0 pb-8 bg-transparent border-t border-neutral-900/10 dark:border-white/30 transition-all duration-300 ease-in-out lg:absolute lg:bottom-0 lg:left-0 lg:p-8 lg:w-full lg:h-full lg:border-none lg:bg-gradient-to-t from-black/40 to-transparent lg:hover:bg-none lg:translate-y-10 lg:group-hover:translate-y-0">
+        <h2 className="font-serif text-lg text-black dark:text-white lg:text-white lg:group-hover:text-black lg:dark:group-hover:text-white lg:border-b lg:group-hover:border-neutral-900/10 lg:dark:group-hover:border-white/30 border-white/30 lg:h-12 lg:mb-2">
           {item.data.title.text}
         </h2>
-        <p className="text-sm text-neutral-800 lg:text-neutral-300 lg:group-hover:text-neutral-800 lg:dark:group-hover:text-neutral-300 lg:opacity-0 lg:group-hover:opacity-100 lg:translate-y-4 lg:group-hover:translate-y-0 transition-all delay-75 group-hover:delay-200">
+        <p className="text-sm text-neutral-800 dark:text-neutral-300 lg:text-neutral-300 lg:group-hover:text-neutral-800 lg:dark:group-hover:text-neutral-300 lg:opacity-0 lg:group-hover:opacity-100 lg:translate-y-4 lg:group-hover:translate-y-0 transition-all delay-75 group-hover:delay-200">
           {item.data.subtitle.text}
         </p>
       </div>
