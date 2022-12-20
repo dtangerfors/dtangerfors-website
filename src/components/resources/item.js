6d6b0e093eb3afb8f6cab28d85d368db6aa315b1
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2022-12-20 15:54:19 +0100

    Added padding

--- src/components/resources/item.js
+++ src/components/resources/item.js
@@ -14,20 +14,23 @@ const ResourceItems = ({ items }) => {
         animate="visible"
         transition={transition}
         key={i}
-        className="bg-neutral-200 dark:bg-neutral-900 p-12 rounded-[1rem] h-full"
+        className="bg-neutral-200 dark:bg-neutral-900 rounded-[1rem] h-full overflow-hidden"
       >
-        <H3 className="pt-0">
+        <div className="bg-neutral-200 dark:bg-neutral-800">
+        <H3 className="pt-0 pb-0">
           <a
             href={resource.item_link.url}
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center text-inherit hover:text-transparent bg-clip-text hover:bg-gradient-to-r from-[#ae5ae0] via-[#ffcb00] to-[#ff5500]"
+            className="flex items-center text-inherit px-12 py-8 leading-none hover:text-transparent bg-clip-text hover:bg-gradient-to-r from-[#ae5ae0] via-[#ffcb00] to-[#ff5500]"
           >
             {resource.item_title.text}
             <i className="ri-external-link-line text-base align-baseline ml-auto"></i>
           </a>
         </H3>
-        <div className="pt-8">
+        </div>
+        
+        <div className="p-12 pt-8">
           <Paragraph>{resource.item_desc.text}</Paragraph>
         </div>
       </motion.div>
